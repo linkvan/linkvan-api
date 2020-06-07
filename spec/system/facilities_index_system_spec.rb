d31b451f6d60aec0df07359e16b654b5808af5ec
Fabio Lima <fabionl@gmail.com>
2020-06-07 13:02:39 -0700

    Added an Initial very simplified system spec for Facilities Index.

---
+++ spec/system/facilities_index_system_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+# To set type of spec:
+#   RSpec.describe "Hello", type: :feature do
+#
+RSpec.describe "Facilities index" do
+  it 'returns a json object' do
+    visit api_facilities_path
+
+    expect(page.status_code).to be(200)
+    # expect(page).to have_text("Hello#index")
+  end
+end
