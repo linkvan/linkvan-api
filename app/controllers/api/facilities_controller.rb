313d8388040a1e3cb3a0d04552e89d123741aa0d
Fabio Lima <fabionl@gmail.com>
2021-07-25 10:38:05 -0700

    [API] Fixes a crash on Facilities show endpoint. (#41)
    * [API] Fixes a crash on Facilities show endpoint.
    
    * [API] Changes Facilities show endpoint's generated JSON structure.

--- app/controllers/api/facilities_controller.rb
+++ app/controllers/api/facilities_controller.rb
@@ -7,13 +7,15 @@ class Api::FacilitiesController < Api::BaseController
     @facilities = @facilities.with_service(params[:service]) if params[:service].present?
 
     @response = FacilitiesSerializer.new(@facilities, Facilities::IndexFacilitySerializer)
+
     render json: @response, status: :ok
   end
 
   # GET /facilities/:id
   def show
     @facility = Facility.find(params[:id])
-    @response = FacilitySerializer.new(@facility)
+    @response = { facility: FacilitySerializer.new(@facility).as_json }
+
     render json: @response, status: :ok
   end
 end
