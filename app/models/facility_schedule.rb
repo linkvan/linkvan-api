b631c6c79f0e3d3cbf36adcda70eb0632df7ae66
Fabio Lima <fabionl@gmail.com>
2021-09-19 11:52:39 -0700

    [#6] Fixes logic for FacilitySchedule.availability and scopes. Adds FacilitySchedule.update_schedule_availability method to update related columns based on current schedule data.

--- app/models/facility_schedule.rb
+++ app/models/facility_schedule.rb
@@ -15,32 +15,41 @@ class FacilitySchedule < ApplicationRecord
   }
 
   validates :week_day, presence: true, uniqueness: { scope: :facility_id }
-
   validate :time_slots_presence
+
+  attribute :closed_all_day, :boolean, default: -> { true }
+  attribute :open_all_day, :boolean, default: -> { false }
+
+  # after_initialize :set_defaults, if: :new_record?
 
   scope :open_all_day, -> { where(open_all_day: true) }
   scope :closed_all_day, -> { where(closed_all_day: true) }
-  scope :set_time, -> { where(open_all_day: false).where(closed_all_day: false) }
+  scope :set_times, -> { join(:time_slots).where(open_all_day: false) }
 
   def availability
     return :open if open_all_day?
-    return :closed if closed_all_day?
+    return :set_times if time_slots.present?
 
-    :set_times
+    :closed
+  end
+
+  def update_schedule_availability
+    if time_slots.present?
+      update(closed_all_day: false, open_all_day: false)
+    else
+      update(closed_all_day: true, open_all_day: false) unless open_all_day?
+    end
   end
 
   private
 
-    SLOT_TIME_PRESENCE_ERROR = "must not be present if facility availability is %{availability} all day for %{week_day}"
+  SLOT_TIME_PRESENCE_ERROR = "must not be present if facility availability is %{availability} all day for %{week_day}".freeze
 
-    def time_slots_presence
-      # errors.add(:slot_times, "must not be present if facility schedule availability is open all day") if open_all_day?
-      # errors.add(:slot_times, "must not be present if facility schedule availability is closed all day") if closed_all_day?
+  def time_slots_presence
+    open_error_msg = format(SLOT_TIME_PRESENCE_ERROR, availability: :open, week_day: week_day)
+    closed_error_msg = format(SLOT_TIME_PRESENCE_ERROR, availability: :closed, week_day: week_day)
 
-      open_error_msg = SLOT_TIME_PRESENCE_ERROR % { availability: :open, week_day: week_day }
-      closed_error_msg = SLOT_TIME_PRESENCE_ERROR % { availability: :closed, week_day: week_day }
-
-      errors.add(:slot_times, open_error_msg) if open_all_day? && time_slots.present?
-      errors.add(:slot_times, closed_error_msg) if closed_all_day? && time_slots.present?
-    end
+    errors.add(:slot_times, open_error_msg) if open_all_day? && time_slots.present?
+    errors.add(:slot_times, closed_error_msg) if closed_all_day? && time_slots.present?
+  end
 end
