8d84112bd295ef87b5dee69c411a7127001e7dc5
Fabio Lima <fabionl@gmail.com>
2021-10-10 13:05:00 -0700

    [#6] Implements update and destroy actions of Admin::FacilitiesController.

--- app/controllers/admin/facilities_controller.rb
+++ app/controllers/admin/facilities_controller.rb
@@ -1,18 +1,36 @@
 # frozen_string_literal: true
 
 class Admin::FacilitiesController < Admin::BaseController
-  # before_action :set_default_request_format
   before_action :load_facilities, only: [:index]
-  before_action :load_facility, only: %i[edit show destroy]
+  before_action :load_facility, only: %i[show edit update destroy]
 
   def index
-    # flash.now[:notice] = "notice test"
-    # flash.now[:alert] = "error test"
   end
 
   def show; end
 
   def edit; end
+
+  def update
+    if @facility.update(facility_params)
+      redirect_to [:admin, @facility], notice: "Successfully updated facility (id: #{@facility.id})"
+    else
+      flash.now[:alert] = "Failed to update facility (id: #{@facility.id}). Errors: #{@facility.errors.full_messages.join('; ')}"
+
+      render action: :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @facility.destroy
+      flash[:notice] = "Successfully deleted Facility #{@facility.name} (id: #{@facility.id}"
+    else
+      # Error when turning Welcome on.
+      flash[:error] = "Failed to delete Facility #{@facility.name} (id: #{@facility.id}). Errors: #{@facility.errors.full_messages.join('; ')}"
+    end
+
+    redirect_back fallback_location: admin_facilities_path
+  end
 
   private
 
@@ -22,10 +40,10 @@ class Admin::FacilitiesController < Admin::BaseController
   end
 
   def load_facility
-    @facility = Facility.find_by(id: params[:id])
+    @facility = Facility.find(params[:id])
   end
 
-  # def set_default_request_format
-  # request.format = :json unless params[:fomat]
-  # end
+  def facility_params
+    params.require(:facility).permit(:verified, :name, :phone, :website, :address, :lat, :long, :description, :notes)
+  end
 end
