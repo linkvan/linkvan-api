1907536935af1aaa95518abc5bdfacfb7d60e26c
Fabio Lima <fabionl@gmail.com>
2021-10-31 12:21:35 -0700

    [#7] removes password_digest column from users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_153348) do
+ActiveRecord::Schema.define(version: 2021_10_31_165820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -176,7 +176,6 @@ ActiveRecord::Schema.define(version: 2021_10_24_153348) do
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
-    t.string "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean "admin", default: false
