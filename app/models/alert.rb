6024185a7880d78c3e03993db448d3487ed103c4
Fabio Lima <fabionl@gmail.com>
2021-11-28 13:32:57 -0800

    [#47] Add ActiveText to Alert model with validation to avoid attachments.

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Alert < ApplicationRecord
+  has_rich_text :content
+
   validates :title, :content, presence: true
+  # Disable ability to add attachments to ActionText (needed because ActiveStorage is not currently setup.
+  validates :content, no_attachments: true
 
   scope :timeline, -> { order(updated_at: :desc) }
   scope :active, -> { where(active: true) }
