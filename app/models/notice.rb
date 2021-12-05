0ed58ec6f3a45e7af09abead720bea6ff9322c9d
Fabio Lima <fabionl@gmail.com>
2021-12-05 13:39:28 -0800

    [#49] Changes Notice model to use ActionText for its content.

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Notice < ApplicationRecord
+  has_rich_text :content
+
   enum notice_type: {
     general: "general",
     covid19: "covid19",
@@ -8,5 +10,20 @@ class Notice < ApplicationRecord
     cooling_center: "cooling_center"
   }
 
+  validates :title, :content, :slug, presence: true
+  validates :slug, uniqueness: true
+  # Disable ability to add attachments to ActionText (needed because ActiveStorage is not currently setup.
+  validates :content, no_attachments: true
+
+  before_validation :set_slug
+
+  scope :timeline, -> { order(updated_at: :desc) }
   scope :published, -> { where(published: true) }
+  scope :draft, -> { where(published: false) }
+
+  private
+
+  def set_slug
+    self.slug = title.to_s.parameterize
+  end
 end
