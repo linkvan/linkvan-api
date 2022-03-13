e8c993a654b06553958feb5486ea3d910eeb2a7d
Fabio Lima <fabionl@gmail.com>
2022-03-13 11:43:36 -0700

    Adds key column to services. Adds Service#key and welcomes to facilities API.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_183141) do
+ActiveRecord::Schema.define(version: 2022_03_13_180118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -187,6 +187,8 @@ ActiveRecord::Schema.define(version: 2021_12_05_183141) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "key", null: false
+    t.index ["key"], name: "index_services_on_key", unique: true
     t.index ["name"], name: "index_services_on_name", unique: true
   end
 
