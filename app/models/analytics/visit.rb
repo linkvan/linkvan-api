52bb24decd9de119886446b8b1c126680e7f6b14
Fabio Lima <fabionl@gmail.com>
2022-11-13 12:23:04 -0800

    [#90] Moves Visit model to the Analytics namespace.

--- app/models/analytics/visit.rb
+++ app/models/analytics/visit.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Visit < ApplicationRecord
+class Analytics::Visit < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :impressions, through: :events
 
