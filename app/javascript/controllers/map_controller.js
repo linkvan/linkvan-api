3751bf964716da064a4239107ced604f1b219023
jstet <mail@jstet.net>
2023-11-04 14:59:41 -0700

    change map center

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,9 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
 import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
+import { useGeographic } from 'ol/proj';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
+import {Heatmap as HeatmapLayer} from 'ol/layer.js';
 
+useGeographic();
+
+
+const vector = new HeatmapLayer({
+    source: new VectorSource({
+      url: 'data/kml/2012_Earthquakes_Mag5.kml',
+      format: new KML({
+        extractStyles: false,
+      }),
+    }),
+    blur: parseInt(blur.value, 10),
+    radius: parseInt(radius.value, 10),
+    weight: function (feature) {
+      // 2012_Earthquakes_Mag5.kml stores the magnitude of each earthquake in a
+      // standards-violating <magnitude> tag in each Placemark.  We extract it from
+      // the Placemark's name instead.
+      const name = feature.get('name');
+      const magnitude = parseFloat(name.substr(2));
+      return magnitude - 5;
+    },
+  });
 
 export default class extends Controller {
   connect() {
@@ -17,8 +40,8 @@ export default class extends Controller {
           }),
         ],
         view: new View({
-          center: [0, 0],
-          zoom: 2,
+          center: [-123.11782250644546, 49.28062873449969],
+          zoom: 13,
         }),
       });
   }
