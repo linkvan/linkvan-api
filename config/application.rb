7bee8eeaae8e24886358c50caa2e86b7e67be339
Fabio Lima <fabionl@gmail.com>
2025-06-15 12:54:28 -0700

    Update application configuration to disable autoloaded paths in load path and set default headers

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module LinkvanApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # No longer add autoloaded paths into `$LOAD_PATH`. This means that you won't be able
+    # to manually require files that are managed by the autoloader, which you shouldn't do anyway.
+    #
+    # This will reduce the size of the load path, making `require` faster if you don't use bootsnap, or reduce the size
+    # of the bootsnap cache if you use it.
+    config.add_autoload_paths_to_load_path = false
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
