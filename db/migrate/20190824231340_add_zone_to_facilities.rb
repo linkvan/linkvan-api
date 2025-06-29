7527626954290fe67641c73d94981c312dc84dda
Fabio Lima <fabionl@gmail.com>
2025-06-28 19:05:52 -0700

    Fix migration to use MigrationFacility class for updating zone references

--- db/migrate/20190824231340_add_zone_to_facilities.rb
+++ db/migrate/20190824231340_add_zone_to_facilities.rb
@@ -1,4 +1,8 @@
 class AddZoneToFacilities < ActiveRecord::Migration[4.2]
+  class MigrationFacility < ActiveRecord::Base
+    self.table_name = 'facilities'
+  end
+
   def change
     add_reference :facilities, :zone, index: true, foreign_key: true
 
@@ -9,11 +13,11 @@ class AddZoneToFacilities < ActiveRecord::Migration[4.2]
     reversible do |dir|
       dir.up do
         zone = Zone.create(name: zone_name, description: zone_description)
-        Facility.update_all({ zone_id: zone.id })
+        MigrationFacility.update_all({ zone_id: zone.id })
       end
 
       dir.down do
-        Facility.update_all({ zone_id: nil })
+        MigrationFacility.update_all({ zone_id: nil })
         zone = Zone.find_by(name: zone_name)
         zone.destroy
       end
