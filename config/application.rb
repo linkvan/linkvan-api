69e2dbf051e3e7513a206c7f26b05c4d813aa5b6
Fabio Lima <fabionl@gmail.com>
2025-09-14 09:28:49 -0700

    Finish going through each new_framework_defaults initializer

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module LinkvanApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # cache entry format version
+    config.active_support.cache_format_version = 7.1
+
     # No longer add autoloaded paths into `$LOAD_PATH`. This means that you won't be able
     # to manually require files that are managed by the autoloader, which you shouldn't do anyway.
     #
